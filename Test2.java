acf1c7588588db8fe41a1b1aa6618741daf5d6b2
ashushmohit <ashishthakur5280@gmail.com>
2024-03-30 12:53:14 +0530

    firstcommit

---
+++ Test2.java
@@ -0,0 +1,12 @@
+class Test2 {
+public static void main(String[] args) { 
+int a =5;
+int b=5;
+int sum = a+b;
+int Multi =a*b;
+int divide =a/b;
+System.out.println(sum);
+System.out.println(Multi);
+System.out.println(divide);
+}
+}
---
+++ selfintroduction.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selfintroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//chrome - chrome driver - Method close get
+		//firefox - FIREFOX driver - Method close get
+		//Webdrive - interface
+		System.setProperty("webdriver.chrome.driver", "E:\\setup\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
