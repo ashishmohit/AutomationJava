7f2b36a5e98640d1c4840c1088aeb8c8a4597fb9
ashushmohit <ashishthakur5280@gmail.com>
2024-03-30 13:11:05 +0530

    firstcommit1

--- Test2.java
+++ Test2.java
@@ -2,10 +2,13 @@ class Test2 {
 public static void main(String[] args) { 
 int a =5;
 int b=5;
+int c=5;
 int sum = a+b;
 int Multi =a*b;
 int divide =a/b;
+int sum1 = a+b+c;
 System.out.println(sum);
+System.out.println(sum1);
 System.out.println(Multi);
 System.out.println(divide);
 }
